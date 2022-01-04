42d1c44647fa16af06a4b19d1cbcda2d045ab104
nouria chaouy <nouria.chaouy@gmail.com>
2022-01-04 18:31:33 +0000

    initialised firebase, added hooks and context plus created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// page components
+import Create from './pages/create/Create';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Signup from './pages/signup/Signup';
+
 
 function App() {
   return (
     <div className="App">
-
+       <BrowserRouter>
+       <div className='container'>
+         <Routes>
+         <Route path="/" element={<Dashboard />}/>
+         <Route path="/create" element={<Create />}/>
+         <Route path="/login" element={<Login />}/>
+         <Route path="/signup" element={<Signup />}/>
+         <Route path="/projects/:id" element={<Project />}/>
+       </Routes>
+       </div>
+      </BrowserRouter>
     </div>
   );
 }
